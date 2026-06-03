1d9f2263e3e5b84fde1f424ca6068003a3edf311
luoqiucc <luoqiucc@outlook.com>
2026-06-03 14:31:11 +0800

    update

--- src/main.c
+++ src/main.c
@@ -19,6 +19,8 @@
 
 #define RED_BOLD "\033[1;31m"
 #define GREEN_BOLD "\033[1;32m"
+#define BLUE_BOLD "\033[1;34m"
+#define YELLOW_BOLD "\033[1;33m"
 #define BOLD_ON "\033[1m"
 #define RESET "\033[0m"
 
